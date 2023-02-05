23424e75b0845eb0859ad448d293d0034867b905
manitofighh <manitofigh@protonmail.com>
2023-02-04 22:20:59 -0500

    changed code path

--- dice.js
+++ dice.js
@@ -1,5 +1,5 @@
 const diceButton = document.getElementsByClassName("refresh-button")[0];
-const diceAudio = new Audio("../Sounds/gamemisc_dice-roll-on-wood_jaku5-37414-[AudioTrimmer.com].mp3"); // Dice sound
+const diceAudio = new Audio("Sounds/gamemisc_dice-roll-on-wood_jaku5-37414-[AudioTrimmer.com].mp3"); // Dice sound
 // const diceAudio = new Audio("../Sounds/android2.mp3"); // Android sound
 // const diceAudio = new Audio("../Sounds/slowAugh.mp3"); // Augh sound
 
@@ -9,7 +9,7 @@ diceButton.addEventListener("click", function() {
   var randomDice2 = Math.floor(Math.random() * 6) + 1;
   var diceImage2 = "round" + randomDice2 + ".svg";
   diceAudio.play();
-  document.getElementsByTagName("img")[0].setAttribute("src", "../Images/" + diceImage1);
-  document.getElementsByTagName("img")[1].setAttribute("src", "../Images/" + diceImage2);
+  document.getElementsByTagName("img")[0].setAttribute("src", "Images/" + diceImage1);
+  document.getElementsByTagName("img")[1].setAttribute("src", "Images/" + diceImage2);
 //   location.reload();
 });
